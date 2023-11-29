48761b0e1c1b731cddb11d9c286c47a46b6b19a6
tmh9x <thomas.hermanu@gmail.com>
2023-11-29 18:21:24 +0100

    add delete item

--- src/main.py
+++ src/main.py
@@ -23,9 +23,17 @@ def create_server_connection():
         print("Error, `{err}`")
     return cnx
 
+def getDataFromDB(cnx):
+    cursor = cnx.cursor()
+    query = "SELECT * FROM items;"
+    cursor.execute(query)
+    items = cursor.fetchall()
+    return items
+
+
 def insertIntoDB(cnx, item):   
     cursor = cnx.cursor()
-    query = "INSERT INTO items (item) VALUES (%s)"
+    query = "INSERT INTO items (item) VALUES (%s);"
     data = [item]
     try:
         cursor.execute(query, data)
@@ -34,15 +42,36 @@ def insertIntoDB(cnx, item):
     except Error as err:
         print(f"Error: '{err}'")
 
+def deleteFromDB(cnx, id):
+    cursor = cnx.cursor()
+    query = "DELETE FROM items WHERE id = %s;"
+    id = [id]
+    try:
+        cursor.execute(query, id)
+        cnx.commit()
+        print("Data deleted successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
 @app.get("/", response_class=HTMLResponse)
 def get_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    cnx = create_server_connection()
+    items = getDataFromDB(cnx)
+    print("ITEMS", items)
+    return templates.TemplateResponse("index.html", {"request": request, "items": items})
 
-@app.post("/", response_class=RedirectResponse)
-def post_db(item: Annotated[str, Form()]):
+@app.post("/additem", response_class=RedirectResponse)
+def post_insertIntoDB(item: Annotated[str, Form()]):
     cnx = create_server_connection()
     print("CONNECTION", cnx)
     insertIntoDB(cnx, item)
+    return RedirectResponse(url="http://127.0.0.1:8000", status_code=303)
+
+@app.post("/deleteitem", response_class=RedirectResponse)
+def post_deleteFromDB(id: Annotated[str, Form()]):
+    cnx = create_server_connection()
+    print("CONNECTION", cnx)
+    deleteFromDB(cnx, id)
     return RedirectResponse(url="http://127.0.0.1:8000", status_code=303)
 
 if __name__ == "__main__":
