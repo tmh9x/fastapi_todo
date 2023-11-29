280d1b80640f97f8839c7fb4cbe17ecf4dda0f2a
tmh9x <thomas.hermanu@gmail.com>
2023-11-29 12:14:42 +0100

    remove venv

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
-app = FastAPI(title="To-Do-App", version="0.0.1", openapi_url="/openapi.json")
+app = FastAPI()
+
+templates = Jinja2Templates(directory="./templates")
+
+@app.get("/", response_class=HTMLResponse)
+def get_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
