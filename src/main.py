b6489acce9ad36b23b16c8388761d88388ededbe
tmh9x <thomas.hermanu@gmail.com>
2023-12-01 12:22:47 +0100

    rm .env and set env variables

--- src/main.py
+++ src/main.py
@@ -1,23 +1,27 @@
+import os
 from typing import Annotated
 from fastapi import FastAPI, APIRouter, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from mysql.connector import Error
+from dotenv import load_dotenv
 import mysql.connector
 
 app = FastAPI()
+load_dotenv()
 
 templates = Jinja2Templates(directory="./templates")
 
 # CREATE SERVER CONNECTION
 def create_server_connection():
     cnx = None
-    DB_USER="todo"
-    DB_PASSWORD="1234"
-    DB_HOST="127.0.0.1"
-    DB_DATABASE="todo"
     try:
-        cnx = mysql.connector.connect(user=DB_USER, password=DB_PASSWORD, host=DB_HOST, database=DB_DATABASE)
+        cnx = mysql.connector.connect(
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            host=os.getenv('DB_HOST'),
+            database=os.getenv('DB_DATABASE')
+            )
         print("Database connection successful")
     except Error as err:
         print("Error, `{err}`")
@@ -86,24 +90,24 @@ def post_insertIntoDB(item: Annotated[str, Form()]):
     cnx = create_server_connection()
     print("CONNECTION", cnx)
     insertIntoDB(cnx, item)
-    return RedirectResponse(url="http://127.0.0.1:8000", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 @app.post("/deleteitem", response_class=RedirectResponse)
 def post_deleteFromDB(id: Annotated[str, Form()]):
     cnx = create_server_connection()
     print("CONNECTION", cnx)
     deleteFromDB(cnx, id)
-    return RedirectResponse(url="http://127.0.0.1:8000", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 @app.post("/updateitem", response_class=RedirectResponse)
 def post_updateFromDB(id: Annotated[str, Form()]):
     cnx = create_server_connection()
     print("CONNECTION", cnx)
     updateFromDB(cnx, id)
-    return RedirectResponse(url="http://127.0.0.1:8000", status_code=303)
+    return RedirectResponse(url="/", status_code=303)
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=os.getenv('UVICORN_HOST'), port=8000)
 
